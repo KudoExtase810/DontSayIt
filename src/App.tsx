ea4449d754fb4b95f5847f7a7c0d98d017b1fa88
KudoExtase810 <alaeddino4892@gmail.com>
2023-09-14 21:50:50 +0100

    organized asset imports

--- src/App.tsx
+++ src/App.tsx
@@ -5,43 +5,40 @@ import WinningScreen from "./components/WinningScreen";
 import { Howler } from "howler";
 // @ts-ignore
 import Preload from "react-preload";
-
-// winning images
-import onesc from "./assets/images/1-social-credit.jpg";
-import ftsc from "./assets/images/15-social-credit.jpg";
-import nnsc from "./assets/images/999999-social-credit.jpg";
-import smurfcat from "./assets/images/smurfcat.jpg";
-import lightskin from "./assets/images/lightskin.jpg";
-import malphitels from "./assets/images/malhpitels.jpg";
-import lightskinar from "./assets/images/lightskin-ar.jpg";
-
-// trollface frames
-import frame1 from "./assets/images/trollface-frames/frame1.jpg";
-import frame2 from "./assets/images/trollface-frames/frame2.jpg";
-import frame3 from "./assets/images/trollface-frames/frame3.jpg";
-import frame4 from "./assets/images/trollface-frames/frame4.jpg";
-import frame5 from "./assets/images/trollface-frames/frame5.jpg";
-
-// on racism images & gifs
-import minus30sc from "./assets/images/-30-social-credit.jpg";
-import minus69420sc from "./assets/images/-69420-social-credit.jpg";
-import fnafnword from "./assets/images/fnaf-did-u-say-nword.webp";
-import goku from "./assets/images/goku.jpeg";
-import sonic from "./assets/images/sonic.jpg";
-import therock from "./assets/gifs/the-rock.gif";
-import trollface from "./assets/gifs/trollface.gif";
-
-// on racism audio
-import boom from "./assets/audio/boom-super-bass.mp3";
-import prowler from "./assets/audio/goku-prowler.mp3";
-import fnaf from "./assets/audio/fnaf-hallway.mp3";
-import amongus from "./assets/audio/amongus-sus.mp3";
-import trollface_creepy from "./assets/audio/trollface-smile.mp3";
-import sc_siren from "./assets/audio/social-credits-siren.mp3";
-import spiderman from "./assets/audio/spiderman-migue-ohara.mp3";
-import chineseman from "./assets/audio/chinese-man.mp3";
-
 import r from "./utils/randomize";
+
+import * as mostAssets from "./mostAssets";
+
+// You can access them like this:
+const {
+    onesc,
+    ftsc,
+    nnsc,
+    smurfcat,
+    lightskin,
+    malphitels,
+    lightskinar,
+    frame1,
+    frame2,
+    frame3,
+    frame4,
+    frame5,
+    minus30sc,
+    minus69420sc,
+    fnafnword,
+    goku,
+    sonic,
+    therock,
+    trollface,
+    boom,
+    prowler,
+    fnaf,
+    amongus,
+    trollface_creepy,
+    sc_siren,
+    spiderman,
+    chineseman,
+} = mostAssets;
 
 function App() {
     const [isRacist, setIsRacist] = useState(false);
@@ -73,6 +70,8 @@ function App() {
         </div>
     );
 
+    // The images we wanna initally load so that the user only has to wait for them
+    // the first time they enter the website
     const allImagesToLoad = [
         frame1,
         frame2,
---
+++ src/mostAssets.ts
@@ -0,0 +1,34 @@
+// winning images
+export { default as onesc } from "./assets/images/1-social-credit.jpg";
+export { default as ftsc } from "./assets/images/15-social-credit.jpg";
+export { default as nnsc } from "./assets/images/999999-social-credit.jpg";
+export { default as smurfcat } from "./assets/images/smurfcat.jpg";
+export { default as lightskin } from "./assets/images/lightskin.jpg";
+export { default as malphitels } from "./assets/images/malhpitels.jpg";
+export { default as lightskinar } from "./assets/images/lightskin-ar.jpg";
+
+// trollface frames
+export { default as frame1 } from "./assets/images/trollface-frames/frame1.jpg";
+export { default as frame2 } from "./assets/images/trollface-frames/frame2.jpg";
+export { default as frame3 } from "./assets/images/trollface-frames/frame3.jpg";
+export { default as frame4 } from "./assets/images/trollface-frames/frame4.jpg";
+export { default as frame5 } from "./assets/images/trollface-frames/frame5.jpg";
+
+// on racism images & gifs
+export { default as minus30sc } from "./assets/images/-30-social-credit.jpg";
+export { default as minus69420sc } from "./assets/images/-69420-social-credit.jpg";
+export { default as fnafnword } from "./assets/images/fnaf-did-u-say-nword.webp";
+export { default as goku } from "./assets/images/goku.jpeg";
+export { default as sonic } from "./assets/images/sonic.jpg";
+export { default as therock } from "./assets/gifs/the-rock.gif";
+export { default as trollface } from "./assets/gifs/trollface.gif";
+
+// on racism audio
+export { default as boom } from "./assets/audio/boom-super-bass.mp3";
+export { default as prowler } from "./assets/audio/goku-prowler.mp3";
+export { default as fnaf } from "./assets/audio/fnaf-hallway.mp3";
+export { default as amongus } from "./assets/audio/amongus-sus.mp3";
+export { default as trollface_creepy } from "./assets/audio/trollface-smile.mp3";
+export { default as sc_siren } from "./assets/audio/social-credits-siren.mp3";
+export { default as spiderman } from "./assets/audio/spiderman-migue-ohara.mp3";
+export { default as chineseman } from "./assets/audio/chinese-man.mp3";
